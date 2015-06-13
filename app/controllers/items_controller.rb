08deb0428e377d95418bfbecffe062dd63ce2b9f
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-13 18:06:50 +0900

    Add new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,34 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    # formからデータを受け取る
+    @item = Item.new(item_params)
+
+    # これを保存する
+    @item.save
+
+    # show.html.erbに飛ばす
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+
+  def item_params
+    # params.require(:key).permit(:filter)
+    params.require(:item).permit(
+      :name,
+      :price,
+      :seller_id,
+      :description,
+      :email,
+      :image_url
+    )
+  end
+
 end
